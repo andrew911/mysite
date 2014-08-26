e7d184025e9a1b1bcc53e6d7e6731715d29e1654
Andrew Rorer <andrewrorer117@gmail.com>
2014-08-26 03:38:59 -0400

    Added a model for Polls and Choices

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -68,7 +68,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
